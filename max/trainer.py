368e23365cda1c7173c33676c06e491dd79022cd
Max Maton <info@maxmaton.nl>
2018-01-27 22:14:03 +0000

    Finish trainer

--- max/trainer.py
+++ max/trainer.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
+import game_state
 
 class Trainer:
+	def __main__():
+		t = Trainer("/tmp/model")
+		t.queue_sample(GameState())
+		t.set_label(10)
+		t.train()
+
 	def __init__(self, model_dir):
 		self.estimator = tf.estimator.Estimator(
 			model_fn = model.create_model,
@@ -9,7 +16,7 @@ class Trainer:
 			),
 			params = {
 				'size': 12,
-				'regularizer': 0.001
+				'regularizer': 0.001,
 				'numbers_weight': 0.01,
 				'learnrate': 0.003,
 			},
@@ -27,6 +34,15 @@ class Trainer:
 		self.queue = []
 	
 	def train(self):
+		data = (tf.data.Dataset.range(len(self.samples))
+			.map(lambda i: self.samples[i])
+			.repeat()
+			.shuffle(1024)
+			.batch(32))
+
+		iterator = data.make_one_shot_iterator().get_next()
+
 		self.estimator.train(
-			
+			input_fn = iterator,
+			steps = 1000,
 		)
