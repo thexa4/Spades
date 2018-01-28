ba5d4c458fe5c71d0568992e9af8754a441151b0
Max Maton <info@maxmaton.nl>
2018-01-28 00:01:31 +0000

    Fixed trainer

--- max/model.py
+++ max/model.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 
 class Model:
 
@@ -11,40 +12,42 @@ class Model:
 	 - bags
 	 - suits_empty
 	"""
-	def create_network(features, params):
-		bids_hot = tf.concat(tf.one_hot(
+	def create_network(features, mode, params):
+
+		batch_size = tf.shape(features['bids'])[0]
+		bids_hot = tf.reshape(tf.one_hot(
 			indices = features['bids'],
 			depth = 15,
-			dtype = tf.float(),
+			dtype = tf.float32,
 			name = 'bids_hot',
-		), axis = -1)
+		), shape = [batch_size, 15 * 4], name='bids_hot_reshape')
 
-		tricks_hot = tf.concat(tf.one_hot(
+		tricks_hot = tf.reshape(tf.one_hot(
 			indices = features['tricks'],
 			depth = 14,
-			dtype = tf.float(),
+			dtype = tf.float32,
 			name = 'tricks_hot',
-		), axis = -1)
+		), shape = [batch_size, 14 * 4], name='tricks_hot_reshape')
 
-		bags_hot = tf.concat(tf.one_hot(
+		bags_hot = tf.reshape(tf.one_hot(
 			indices = features['bags'],
 			depth = 10,
-			dtype = tf.float(),
+			dtype = tf.float32,
 			name = 'bags_hot',
-		), axis = -1)
+		), shape = [batch_size, 10 * 2], name = 'bags_hot_reshape')
 		
 
-		input_layer = tf.concat([ hand, seen, bids_hot, tricks_hot, bags_hot, suits_empty ], axis = -1)
+		input_layer = tf.concat([ tf.to_float(features['hand']), tf.to_float(features['seen']), bids_hot, tricks_hot, bags_hot, tf.to_float(features['suits_empty']) ], axis = -1)
 
-		hidden_layer = tf.layers.dense(input_layer, params.size, activation = tf.nn.tanh, kernel_regularizer=tf.contrib.layers.l2_regularizers(params.regularizer), use_bias=True)
+		hidden_layer = tf.layers.dense(input_layer, params['size'], activation = tf.nn.tanh, kernel_regularizer=tf.contrib.layers.l2_regularizer(params['regularizer']), use_bias=True)
 
 		dropout = tf.layers.dropout(hidden_layer, training = (mode == tf.estimator.ModeKeys.TRAIN), rate=0.5)
 
-		return tf.multiply(tf.layers.dense(dropout, 1, kernel_regularizer=tf.contrib.layers.l2_regularizer(params.regularizer), use_bias=True, activation=tf.tanh), 300.0)
+		return tf.multiply(tf.layers.dense(dropout, 1, kernel_regularizer=tf.contrib.layers.l2_regularizer(params['regularizer']), use_bias=True, activation=tf.tanh), 300.0)
 
 	def create_predictor(features, mode, params):
 
-		output_layer = Model.create_network(features, params)
+		output_layer = Model.create_network(features, mode, params)
 		
 		predictions_dict = {
 			"number": output_layer,
@@ -57,18 +60,18 @@ class Model:
 		)
 
 	def create_trainer(features, labels, mode, params):
-		output_layer = Model.create_network(features, params)
+		output_layer = Model.create_network(features, mode, params)
 
 		tf.summary.histogram("number/guessed", output_layer)
 
 		loss = 0
 		
-		reg_loss = tf.lsses.get_regularization_loss()
+		reg_loss = tf.losses.get_regularization_loss()
 		loss += reg_loss
-		tf.summary.scalar("loss/regularization", regularization_loss)
+		tf.summary.scalar("loss/regularization", reg_loss)
 
-		target = targets['number']
-		num_loss = tf.losses.mean_squared_error(target, output_layer) * params.numbers_weight
+		target = tf.reshape(labels, tf.shape(output_layer))
+		num_loss = tf.losses.mean_squared_error(target, output_layer) * params['numbers_weight']
 		loss += num_loss
 		tf.summary.scalar("loss/numbers", num_loss)
 
@@ -78,15 +81,15 @@ class Model:
 
 		tf.summary.scalar("loss/sum", loss)
 		eval_metric_ops = {
-			"loss/numbers": num_loss,
-			"loss/regularization": reg_loss,
-			"loss/sum": loss,
+			#"loss/numbers": num_loss,
+			#"loss/regularization": reg_loss,
+			#"loss/sum": loss,
 		}
 
 		train_op = tf.contrib.layers.optimize_loss(
 			loss = loss,
 			global_step = tf.train.get_global_step(),
-			learning_rate = params.learnrate,
+			learning_rate = params['learnrate'],
 			optimizer = "Adagrad",
 		)
 
--- max/trainer.py
+++ max/trainer.py
@@ -1,18 +1,27 @@
 import tensorflow as tf
 import game_state
+from model import Model
+from game_state import GameState
+
+def main():
+	t = Trainer("/tmp/model/")
+	t.queue_sample(GameState(
+		hand = [],
+		seen = [],
+		scores = [0, 0],
+		tricks = [0, 0, 0, 0],
+		bids = [3, 3, 3, 3],
+		empty_suits = [[0, 0, 0, 0]] * 4,
+	))
+	t.set_label(10)
+	t.train()
 
 class Trainer:
-	def __main__():
-		t = Trainer("/tmp/model")
-		t.queue_sample(GameState())
-		t.set_label(10)
-		t.train()
-
 	def __init__(self, model_dir):
 		self.estimator = tf.estimator.Estimator(
-			model_fn = model.create_model,
-			config = tf.learn.RunConfig(
-				model_dir = Model.create_trainer,
+			model_fn = Model.create_trainer,
+			config = tf.estimator.RunConfig(
+				model_dir = model_dir,
 			),
 			params = {
 				'size': 12,
@@ -22,7 +31,16 @@ class Trainer:
 			},
 		)
 
-		self.samples = []
+		self.samples = {
+			"hand": [],
+                        "seen": [],
+                        "bids": [],
+                        "tricks": [],
+                        "scores": [],
+                        "bags": [],
+                        "suits_empty": [],
+		}
+		self.labels = []
 		self.queue = []
 
 	def queue_sample(self, sample):
@@ -30,19 +48,35 @@ class Trainer:
 
 	def set_label(self, label):
 		for sample in self.queue:
-			self.samples.append((sample, label))
+			for key in sample.keys():
+				self.samples[key].append(sample[key])
+			self.labels.append(label)
 		self.queue = []
+
+	def collect_data(self):
+		label = tf.concat(self.labels, axis=-1)
+		data = {}
+		for key in self.samples.keys():
+			data[key] = tf.concat(self.samples[key], axis = -1)
+
+		return (data, label)
 	
-	def train(self):
-		data = (tf.data.Dataset.range(len(self.samples))
-			.map(lambda i: self.samples[i])
+	def input_fn(self):
+		data = (tf.data.Dataset.from_tensors(self.collect_data())
 			.repeat()
 			.shuffle(1024)
 			.batch(32))
 
-		iterator = data.make_one_shot_iterator().get_next()
+		return data.make_one_shot_iterator().get_next()
+		
 
+	def train(self):
 		self.estimator.train(
-			input_fn = iterator,
-			steps = 1000,
+			input_fn = self.input_fn,
+			steps = 100000,
 		)
+
+		print("done")
+
+if __name__ == "__main__":
+	main()
