437081e0297bd07015368350a219bab0b705347a
Max Maton <info@maxmaton.nl>
2018-01-30 02:35:07 +0000

    Print win ratio braindead tensor

--- game_runner.py
+++ game_runner.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 
 from game_manager import GameManager
+from braindead_player import BraindeadPlayer
 from max.tensor_player import TensorPlayer
 from max.predictor import Predictor
 
 def main():
-	players = [TensorPlayer(Predictor(None)) for i in range(4)]
+	t_p = [TensorPlayer(Predictor(None)) for i in range(2)]
+	b_p = [BraindeadPlayer() for i in range(2)]
+	players = [b_p[0], t_p[0], b_p[1], t_p[1]]
 	manager = GameManager(players)
-	print(manager.play_game())
+	rounds = 100
+
+	b_wins = 0
+	t_wins = 0
+	for i in range(1, rounds):
+		score = manager.play_game()
+		if score[0] > score[1]:
+			b_wins += 1
+		if score[1] > score[0]:
+			t_wins += 1
+		print(score)
+	print(str(b_wins) + " - " + str(t_wins))
 
 
 main()
