d4e4105ac098be6a6536d038698d980b5d72c4c9
root <root@spades1.ws.maxmaton.nl>
2021-10-20 16:11:01 +0000

    Updated train script

--- train.sh
+++ train.sh
@@ -4,17 +4,13 @@ q="$1"
 generation="$2"
 
 padded="$(sed -e :a -e 's/^.\{1,2\}$/0&/;ta' <<<"$generation")"
-cores="16"
-blocks="$((256 / cores))"
+cores="64"
 
-for r in $(seq 1 $blocks); do
-	for c in $(seq 1 $cores); do
-		i=$(( r * $cores + c ))
+for c in $(seq 1 256); do
+	i=$(( r * $cores + c ))
 
-		ipadded="$(sed -e :a -e 's/^.\{1,3\}$/0&/;ta' <<<"$i")"
-		if [ ! -f "max2/data/q$q/gen$padded/samples/$ipadded.flat" ]; then
-			python generate-fixed.py "$q" "$generation" "$i" &
-		fi
-	done
-	wait
-done
+	ipadded="$(sed -e :a -e 's/^.\{1,3\}$/0&/;ta' <<<"$i")"
+	if [ ! -f "max2/data/q$q/gen$padded/samples/$ipadded.flat" ]; then
+		echo python3 generate-fixed.py "$q" "$generation" "$i"
+	fi
+done | xargs -d "\n" -n 1 -P "$cores" bash -c
